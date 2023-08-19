c9cb44156e3063d01bd9a1b649a1fc1a20e94e32
kiennd <duykien85@gmail.com>
2023-08-19 15:40:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Arbitrage from './Screen/Arbitrage/Arbitrage';
 
+console.log('hi');
+console.log(process.env.REACT_APP_SECRET_NAME || 'Test env');
 function App() {
   return (
     <div>
